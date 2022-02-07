787ff93d3ee3764a7b768c38bfae4a76a6212528
TAVALKS <AlekseiTetyuev@gmail.com>
2022-02-07 09:55:58 +0500

    get call token

--- auth/verify/views.py
+++ auth/verify/views.py
@@ -1,4 +1,5 @@
 from rest_framework.response import Response
+from django.http import HttpResponseNotFound
 from rest_framework.views import APIView
 from rest_framework.decorators import authentication_classes, permission_classes
 from rest_framework import authentication, permissions
@@ -26,12 +27,19 @@ class GetInfo(APIView):
 
 
 class GetCallToken(APIView):
+    '''View for get call token or generate'''
     authentication_classes = [authentication.TokenAuthentication]
     permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request, format=None):
         user = request.user
-        token = token_hex()
+        try:
+            mc = MakeCall.objects.get(user=user)
+            token = mc.call_token
+        except MakeCall.DoesNotExist:
+            token = token_hex()
+            mc = MakeCall(user=user, call_token=token)
+            mc.save()
         response = Response(
             {'call_token':token,
              'user': user.username}
@@ -46,16 +54,15 @@ class VerifyNumber(APIView):
     def post(self, request):
         tel = request.data['tel']
         token = request.data['token']
+        try:
+            mc = MakeCall.objects.get(call_token=token)
+            user = mc.user
+        except MakeCall.DoesNotExist:
+            return HttpResponseNotFound()
+        requests.post(url='http://127.0.0.1:5000/verify',
+                      json={'phone_number': tel})
         response = Response({
             'token': token,
-            'tel': tel,
+            'tel': tel
         })
-        info = verify_numberSerializer(verify_number.objects.filter(user=2), many=True).data
-        requests.post(url='http://127.0.0.1:5000/verify', 
-                      json={'phone_number': tel})
-        if len(info) > 0:
-            info = info[0]
-        response = Response(
-            {'user': info}
-        )
         return response
